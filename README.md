e5ab1770093a36b77e6da7108afa8cc959c335a7
user00000001 <jaybrownjyb@gmail.com>
2024-05-10 00:30:08 +0800

    add Raffle contract.
    Signed-off-by: user00000001 <jaybrownjyb@gmail.com>

---
+++ README.md
@@ -0,0 +1,7 @@
+# Operations
+
+## commands
+
+```shell
+pnpm i -D hardhat@~2.9.0 @nomiclabs/hardhat-ethers@npm:hardhat-deploy-ethers@0.3.0-beta.13 ethers@^5.0.0 @nomiclabs/hardhat-etherscan @nomiclabs/hardhat-waffle@2.0.0 chai ethereum-waffle@^3.0.0 hardhat hardhat-contract-sizer hardhat-deploy@^0.9.29 hardhat-gas-reporter@^1.0.8 prettier prettier-plugin-solidity solhint solidity-coverage@~0.7.0 dotenv @typechain/ethers-v5@~10.0.0 @typechain/hardhat@~6.0.0 @types/chai @types/node ts-node typechain@~8.0.0 typescript @chainlink/contracts@~0.4.0
+```
---
+++ hardhat.config.ts
@@ -0,0 +1,40 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "hardhat-deploy";
+import "dotenv/config";
+
+const config: HardhatUserConfig = {
+  // solidity: "0.8.18",
+  solidity: {
+    compilers: [
+      {version: "0.8.7"}, {version: "0.6.6"}
+    ]
+  },
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY!],
+      chainId: 11155111,
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      // accounts: [],
+      chainId: 31337,
+    }
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+    user: {
+      default: 0,
+    }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
+  },
+  // gasReporter: {
+  //   enabled: true
+  // }
+};
+
+export default config;
