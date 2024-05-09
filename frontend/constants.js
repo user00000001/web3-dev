047c7c612b95ce8ae713526a7fe403c79b8a34e0
user00000001 <jaybrownjyb@gmail.com>
2024-05-09 15:11:27 +0800

    frontend for fund method.
    Signed-off-by: user00000001 <jaybrownjyb@gmail.com>

---
+++ frontend/constants.js
@@ -0,0 +1,148 @@
+export const contractAddr = "0xb7f8bc63bbcad18155201308c8f3540b07f84f5e";
+export const abi = [
+    {
+      "inputs": [
+        {
+          "internalType": "contract AggregatorV3Interface",
+          "name": "priceFeedAddr",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "constructor"
+    },
+    {
+      "inputs": [],
+      "name": "CallFailure",
+      "type": "error"
+    },
+    {
+      "inputs": [],
+      "name": "ETHNotEnough",
+      "type": "error"
+    },
+    {
+      "inputs": [],
+      "name": "NotOwner",
+      "type": "error"
+    },
+    {
+      "stateMutability": "payable",
+      "type": "fallback"
+    },
+    {
+      "inputs": [],
+      "name": "I_OWNER",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "MINIMUM_USD",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "name": "addressToAmountFunded",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "cheaperWithdraw",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "fund",
+      "outputs": [],
+      "stateMutability": "payable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "name": "funders",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "getVersion",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "priceFeed",
+      "outputs": [
+        {
+          "internalType": "contract AggregatorV3Interface",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "withdraw",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "stateMutability": "payable",
+      "type": "receive"
+    }
+  ]
---
+++ frontend/index.js
@@ -0,0 +1,50 @@
+import { ethers } from "./ethers-5.1.esm.min.js";
+import { abi, contractAddr } from "./constants.js";
+
+const connectButton = document.getElementById("connectButton")
+const fundButton = document.getElementById("fundButton")
+connectButton.onclick = connection;
+fundButton.onclick = fund;
+
+async function connection() {
+    if(window.ethereum) {
+        await window.ethereum.request({method: "eth_requestAccounts"});
+        connectButton.innerText = "Connected!"
+    } else {
+        console.log("No ethereum wallet!")
+        connectButton.innerText = "Please install MetaMask"
+    }
+}
+
+async function fund() {
+    const fund_input = document.getElementById("fund");
+    if(window.ethereum) {
+        await window.ethereum.request({method: "eth_requestAccounts"});
+        connectButton.innerText = "Connected!"
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        console.log(signer);
+        const fundMe = new ethers.Contract(contractAddr, abi, signer);
+        try {
+            const txResp = await fundMe.fund({value: ethers.utils.parseEther(fund_input.value)});
+            // const txRcpt = await txResp.wait(1);
+            await listenForTransactionMine(txResp, provider)
+            console.log("Done")
+        } catch(e) {
+            console.log(e)
+        }
+    } else {
+        console.log("No ethereum wallet!")
+        connectButton.innerText = "Please install MetaMask"
+    }
+}
+
+function listenForTransactionMine(txResp, provider) {
+    console.log(`Mining ${txResp.hash}`)
+    return new Promise((resolve, reject) => {
+        provider.once(txResp.hash, function(txRcpt){
+            console.log(`Completed with ${txRcpt.confirmations} confirmations`)
+            resolve();
+        }) 
+    })
+}
