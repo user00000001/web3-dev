e0c7464672588879fd14d5774258e1b1ac82799c
user00000001 <jaybrownjyb@gmail.com>
2024-06-20 13:00:55 +0800

    wagmi useBalance.
    Signed-off-by: user00000001 <jaybrownjyb@gmail.com>

--- frontend/pages/examples/_components/CustomNav.tsx
+++ frontend/pages/examples/_components/CustomNav.tsx
@@ -6,6 +6,7 @@ function Nav() {
     <div className=" mt-2 flex space-x-5 justify-evenly">
       <Link className=" bg-gray-300 px-3 rounded-md" href={"/examples/modal-hooks"}>Modal Hooks</Link>
       <Link className=" bg-gray-300 px-3 rounded-md" href={"/examples/custom-connectbutton"}>Custom ConnectButton</Link>
+      <Link className=" bg-gray-300 px-3 rounded-md" href={"/examples/send-transaction"}>Send Transaction</Link>
     </div>
   );
 }
--- frontend/pages/examples/send-transaction.tsx
+++ frontend/pages/examples/send-transaction.tsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CustomNav from "./_components/CustomNav";
 import {
   BaseError,
+  useAccount,
+  useBalance,
   useSendTransaction,
   useWaitForTransactionReceipt,
 } from "wagmi";
-import { parseEther } from "viem";
+import { formatUnits, parseEther } from "viem";
 
 function SendTransaction() {
+  const [to, setto] = useState<`0x${string}`>();
+  const { address: from } = useAccount();
+  const { data: from_balance, refetch: from_refetch } = useBalance({
+    address: from,
+  });
+  const { data: to_balance, refetch: to_refetch } = useBalance({ address: to });
   const {
     data: hash,
     error,
@@ -24,42 +32,86 @@ function SendTransaction() {
   }
   const { isLoading: isConfirming, isSuccess: isConfirmed } =
     useWaitForTransactionReceipt({ hash });
+  useEffect(() => {
+    to_refetch();
+    from_refetch();
+  }, [isConfirmed, to_refetch, from_refetch]);
   return (
     <>
       <CustomNav></CustomNav>
       <div className=" mt-4 bg-blue-400 bg-opacity-50 h-full">
         <form
           onSubmit={onSubmit}
-          className=" h-12 border-collapse border border-red-300 flex items-center justify-center space-x-4 w-4/5 mx-auto rounded-md"
+          className=" h-12 border-collapse border border-red-300 flex items-center justify-center space-x-4 mx-auto rounded-md p-8 bg-lime-300"
         >
-          <label htmlFor="address">To: </label>
-          <input
-            type="text"
-            name="address"
-            placeholder="0x..."
-            id="address"
-            required
-            className=" border w-[24rem]"
-          />
-          <label htmlFor="value">Value(ETH): </label>
-          <input
-            type="text"
-            name="value"
-            placeholder="1"
-            id="value"
-            required
-            className=" border w-16"
-          />
-          <button
-            disabled={isPending}
-            type="submit"
-            className=" bg-slate-400 px-5 rounded-lg border border-black hover:scale-105"
-          >
-            {isPending ? "Confirming..." : "Send"}
-          </button>
+          <div className="flex flex-col">
+            <div>From: {from}</div>
+            <div>
+              {"owns "}
+              {from_balance
+                ? formatUnits(from_balance?.value, from_balance?.decimals)
+                : 0}{" "}
+              ETH
+            </div>
+          </div>
+
+          <div className="flex flex-col">
+            <div className="flex">
+              <label htmlFor="address" className="flex flex-col">
+                <div className=" pr-4">To:</div>
+              </label>
+              <input
+                type="text"
+                name="address"
+                placeholder="0x..."
+                id="address"
+                required
+                className=" border w-[24rem]"
+                onChange={(value) => {
+                  let to = value.target.value;
+                  if (to.length == 42 && to.startsWith(`0x`)) {
+                    console.log(`${to}`);
+                    setto(to as `0x${string}`);
+                  }
+                }}
+              />
+            </div>
+            <div>
+              {"owns "}
+              {to_balance
+                ? formatUnits(to_balance?.value, to_balance?.decimals)
+                : 0}{" "}
+              ETH
+            </div>
+          </div>
+          <div className="flex flex-col">
+            <div>
+              <label htmlFor="value">Value(ETH): </label>
+              <input
+                type="text"
+                name="value"
+                placeholder="1"
+                id="value"
+                required
+                className=" border w-16"
+              />
+            </div>
+            <button
+              disabled={isPending}
+              type="submit"
+              className=" bg-slate-400 px-5 rounded-lg border border-black hover:scale-105"
+            >
+              {isPending ? "Confirming..." : "Send"}
+            </button>
+          </div>
         </form>
         <div className=" divide-y-2 divide-pink-400 divide-dashed divide-opacity-50">
-          {hash && <div className="text-purple-800 text-center">Transaction Hash: {hash}</div>}
+          {hash && (
+            <div className="text-purple-800 text-center">
+              Transaction Hash: {hash}
+            </div>
+          )}
+          {isPending && <div className="">Waiting for Submit...</div>}
           {isConfirming && <div className="">Waiting for confirmation...</div>}
           {isConfirmed && <div>Transaction confirmed.</div>}
           {error && (
